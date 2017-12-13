b8cbabdabacfc0c28f41a488dad1f43ce0e4a5fb
George <georLeonard@gmail.com>
2017-12-13 02:53:53 -0800

    added necessary np.absolute to create norming vector, appears to be fine in def unit_vectors
    debugging net_force vector, unit_vectors returning length 6 arrays under some circumstances.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ n_bodies = 6  #bodies
 dim = 3  # (x,y) GLOBAL
 G = 6.674 * 10**-11
 
-pos_array= np.zeros([n_bodies,dim]) # position relative in meters, x,y to arbitrary origin for each of n bodies
+pos_array= np.zeros([n_bodies,dim]) # position relative in meters, x,y,z to arbitrary origin for each of n bodies
 velocities = np.zeros([n_bodies,dim])  # m/s x,y for each of n bodies
 mass_array = np.ones([n_bodies])  # masses for each of n bodies, in kg
 force_array = np.zeros([n_bodies,dim])  # net forces in x,y for each of n bodies
@@ -34,17 +34,19 @@ def forces(obj_index, positions, masses):
 
 def unit_vectors(obj_index, positions):
     """returns unit vector for obj to each other object"""
-    un_normed = positions - positions[obj_index]
-    norming = np.sum(un_normed, axis=1) ** -1
+    un_normed = positions - positions[obj_index]           > 1
+    norming = np.sum(np.absolute(un_normed), axis=1) ** -1  #abs values of array for summing
     normed = norming * np.transpose(un_normed)
     normed = normed.transpose()
     return normed
 
 def net_force_vector(obj_index, positions, masses):
     """computes x,y,z force vector, in Newtons, for given object index"""
-    unit_vector = unit_vectors(obj_index, positions)
+    unit_vector = unit_vectors(obj_index, positions)  #has NaN for obj_index
+    print(unit_vector)
     force_mags = forces(obj_index, positions, masses)
     force_mags = force_mags * unit_vector.transpose()
+    # print(force_mags)
     force_mags = force_mags.transpose()
     force_mags = force_mags[(force_mags == force_mags).all(1)]
     force_mags = np.sum(force_mags, axis = 1)
@@ -54,7 +56,14 @@ def net_force_vector(obj_index, positions, masses):
     # relative position array (pos array - pos[index]
     # solve geometry, x/y y/z z/x (etc.) * force to return obj by 3(dim) vectorized force
 
-
+def delta_VP(velocities, positions, masses, time_slice):
+    """returns two numpy arrays in list Velocities
+    and Positions updated according to time_slice in seconds"""
+    for index, _ in enumerate(positions):  #better way to access index?
+        force_v = net_force_vector(index,positions,masses)
+        velocities[index] += force_v * time_slice / mass_array[index]        ##delta impulse /mass = delta velocity + orig = new velocity, ammended to input
+    positions += positions * velocities * time_slice ### pos * velocity * time = delta positionprobably needs a math/python debugging
+    return [velocities,positions]
 
 
 
@@ -65,11 +74,11 @@ pos_array[2,1]=1.876
 pos_array[0,2]=5.867546546
 pos_array[4,0]=4.3
 
-net_force_vector(1,pos_array, mass_array)
+b = net_force_vector(2,pos_array, mass_array)
+# a = delta_VP(velocities, pos_array,mass_array, 1)
 
-
-
-
+print(b)
+#print(b[1])
 
 # for i in range(4):
 #     force_output = forces(i, pos_array, mass_array)
