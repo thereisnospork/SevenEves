9866c467013ed0f1164a567b5eb48e9057b012a6
George <georLeonard@gmail.com>
2017-12-17 15:58:15 -0800

    changed some inputs, running first eval for later plotting.  Also got PyPy working as interpreter for ~25% time savings

--- Init_and_run.py
+++ Init_and_run.py
@@ -29,12 +29,11 @@ def init_gaus(interval, years, iterations, time_slice=0.1):
         # # force_array = np.zeros([n_bodies,dim], dtype=np.float64)  # net forces in x,y for each of n bodies  ##extraneous
 
         pos_array   =   np.random.normal(0,10,[n_bodies,dim])             #, dtype=np.float64) # position relative in meters, x,y,z to arbitrary origin for each of n bodies
-        velocities  =   np.random.normal(1,1,[n_bodies,dim])             #, dtype=np.float64)  # m/s x,y for each of n bodies
-        mass_array  =   10**15 * np.random.normal(10,1,[n_bodies])        #, dtype=np.float32)  # masses for each of n bodies, in kg
+        velocities  =   0 * np.random.normal(1,1,[n_bodies,dim])             #, dtype=np.float64)  # m/s x,y for each of n bodies
+        mass_array  =   10**1 * np.random.normal(10,1,[n_bodies])        #, dtype=np.float32)  # masses for each of n bodies, in kg
 
         pos_array.astype(np.float64)
         velocities.astype(np.float64)
-        print(type(pos_array[1,1]))
 
 
         main.continuous(velocities,pos_array,mass_array,interval,steps,serial_num, time_slice)
@@ -44,7 +43,7 @@ def init_gaus(interval, years, iterations, time_slice=0.1):
         print(str(end-start) + 's elapsed this loop')
 
 
-init_gaus(604800, 2, 3, time_slice=3600)
+init_gaus(86400, 1, 25, time_slice=1)
 
 
 
