1d0606f382c3ff6ab2aae30da925031125f47f86
George <georLeonard@gmail.com>
2018-01-06 05:00:25 -0800

    two new slicing functions in graph, MVP start to return starting conditions, VP_results to return VP exempting t0

--- graphing.py
+++ graphing.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+#####ONLY WORKS FOR 3D systems as written!!!!!!!!!!
+
+
 system = 'system_0'
 directory = 'data\\'+system +'\\'
 data_files = os.listdir(directory)
@@ -13,8 +16,9 @@ def files_to_times(files):
         times.append(int(title.split('.')[0].split('_')[1]))
     return times
 
-def files_to_MVP(files, directory):
+def files_to_MVP(directory):
     """pulls all the files in the directory into one large cubic numpy array"""
+    files = os.listdir(directory)
     body_count = num_bodies(files, directory)
     master = np.zeros((body_count,7,len(files))) ##ROW | COLS | TIME
     for index, file in enumerate(files):
@@ -23,11 +27,11 @@ def files_to_MVP(files, directory):
 
 def num_bodies(files, directory):
     a = np.genfromtxt(directory + files[0])
-    print(len(a))
+    # print(len(a))
     return len(a)
 
 def masses(MVP):
-    return MVP[:,1,:]
+    return MVP[:,0,:]
 
 def velocities(MVP):
     return MVP[:,1:4,:]
@@ -35,10 +39,15 @@ def velocities(MVP):
 def positions(MVP):
     return MVP[:,4:8,:]
 
+def VP_results(MVP):
+    return MVP[:,1:8,1:-1] #excludes starting conditions
+
+def MVP_start(MVP):
+    return MVP[:,:,0]
+
 def graph_pos(system,init=False, end = False):
     directory = 'data\\' + system + '\\'
-    data_files = os.listdir(directory)
-    mvp = files_to_MVP(data_files, directory)
+    mvp = files_to_MVP(directory)
     # times = files_to_times(data_files)
     m = masses(mvp)
     # v = velocities(mvp)
@@ -56,6 +65,7 @@ def graph_pos(system,init=False, end = False):
         p_x, p_y, p_z = p[:,0], p[:,1], p[:,2] # graphs trace of position over time (no t coloration/axis)
         col = m.flatten()
 
+
     ax.scatter(p_x, p_y, p_z, c=col, marker='o')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -71,8 +81,7 @@ def graph_pos(system,init=False, end = False):
 
 def graph_vel(system,init=False, end = False):
     directory = 'data\\' + system + '\\'
-    data_files = os.listdir(directory)
-    mvp = files_to_MVP(data_files, directory)
+    mvp = files_to_MVP(directory)
     # times = files_to_times(data_files)
     m = masses(mvp)
     v = velocities(mvp)
@@ -98,14 +107,14 @@ def graph_vel(system,init=False, end = False):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     ax.set_title(system+'_vel')
-    print(v)
+    # print(v)
     plt.show()
 
 
 
-graph_pos('system_0', init=True)
-graph_vel('system_0')
-
+# graph_pos('system_0')
+# graph_vel('system_0')
+#
 
 
 
